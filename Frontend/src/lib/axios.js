a89ad7bec77b728dae895f73aa5f7ea7d199770a
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-05-12 17:03:58 +0530

    update axios baseURL to Render backend

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -4,5 +4,5 @@ export const axiosInstance = axios.create({
     baseURL:
         import.meta.env.MODE === "development" ?
         "http://localhost:3000/api/v1" :
-        "/api/v1",
+        "https://task-management-backend-9i5k.onrender.com/api/v1",
 })
